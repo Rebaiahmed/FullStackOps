d5f42f87497273bf5967f29b60a6aa146426e4bc
RebaiAhmed <ahmed.bouhmid94@gmail.com>
2025-03-12 13:29:35 +0100

    fix: introduce storybook to the project

---
+++ apps/full-stack-ops-monorepo/src/UI/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+
+  @Input() label = 'Click Me';
+  @Input() type = 'button';
+
+
+}
