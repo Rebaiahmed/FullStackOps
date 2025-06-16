7554fa0303cfe896d48def0f5b3592ce5dcbeaec
RebaiAhmed <ahmed.bouhmid94@gmail.com>
2025-06-16 13:59:39 +0200

    cleanup angular project

--- apps/full-stack-ops-monorepo/src/app/app.component.ts
+++ apps/full-stack-ops-monorepo/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { SignalsExploreComponent } from '../signals/signals-explore.component';
 
 @Component({
-  imports: [NxWelcomeComponent,SignalsExploreComponent, RouterModule],
+  imports: [SignalsExploreComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'full-stack-ops-monorepo';
-  private unusedVar: string = 'hello';
 }
